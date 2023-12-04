b7c530232de16e44a17ce276c48b509b5a180f8f
yinna <yinnalucky@gmail.com>
2023-12-04 12:03:36 -0500

    a6

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 import express from 'express'
+import session from "express-session";
 import Hello from "./hello.js"
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
 import "dotenv/config";
+import UserRoutes from "./users/routes.js";
 import AssignmentsRoutes from "./assignments/routes.js";
+import mongoose from "mongoose";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express()
-app.use(cors());
+app.use(cors({credentials: true,
+        origin: "http://localhost:3000",
+    }
+));
+
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+app.use(
+    session(sessionOptions)
+);
+
+
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 AssignmentsRoutes(app);
